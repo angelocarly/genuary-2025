637c1c7e65d2d4b5d07caf88d66f76529125e7ac
Angelo Carly <angelo.carly@protonmail.com>
2025-01-01 14:59:23 +0100

    Initial commit

---
+++ 1-lines/src/main.rs
@@ -0,0 +1,31 @@
+use kiyo::app::app::{App, AppConfig};
+use kiyo::app::draw_orch::{DispatchConfig, DrawConfig, Pass};
+
+fn main() {
+
+    let app_config = AppConfig {
+        width: 1000,
+        height: 1000,
+        vsync: true,
+        log_fps: true,
+    };
+    let app = App::new(app_config);
+
+    let mut draw_config = DrawConfig::new();
+    draw_config.passes = Vec::from([
+        Pass {
+            shader: "1-lines/shaders/fractal.comp".to_string(),
+            dispatches: DispatchConfig::FullScreen,
+            input_resources: Vec::from( [] ),
+            output_resources: Vec::from([ 0 ]),
+        },
+        Pass {
+            shader: "1-lines/shaders/overlay.comp".to_string(),
+            dispatches: DispatchConfig::FullScreen,
+            input_resources: Vec::from( []),
+            output_resources: Vec::from([ 0 ]),
+        },
+    ]);
+
+    app.run(draw_config);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Genuary 2025
+
+This repository contains a Rust workspace with the projects for genuary 2025. I am creating these with [kiyo](https://github.com/angelocarly/kiyo).
